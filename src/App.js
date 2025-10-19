799716625e969e60364abd39dc5fd37ebd4c456e
Shreyas <shreyasacharya8@gmail.com>
2025-10-19 13:28:15 +0100

    wrote entire code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  let [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  let onChangeHandler = (event) => {
+    console.log(task);
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  let onCheckHandler = (event) => {
+    console.log(tasks);
+  };
+
+  let onDeleteHandler = (i) => {
+    setTasks(() =>
+      tasks.filter((taskitem, index) => {
+        return index !== i;
+      })
+    );
+  };
   return (
     <>
       <div className="create">
-        <input placeholder="Enter your task"></input>
-        <button>Enter</button>
+        <input
+          placeholder="Enter your task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              onChangeHandler();
+            }
+          }}
+        ></input>
+        <button onClick={onChangeHandler}>Enter</button>
+        <button onClick={onCheckHandler}>yoho</button>
+      </div>
+      <div>
+        <ui>
+          {tasks.map((t, index) => (
+            <span>
+              <li key={index}>
+                {t}
+                <button onClick={() => onDeleteHandler(index)}>Delete</button>
+              </li>
+            </span>
+          ))}
+        </ui>
       </div>
     </>
   );
